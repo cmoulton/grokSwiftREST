65418cd2fcad21a47feb436dc7ebfa3aca648863
Christina Moulton <christina@teakmobile.com>
2015-09-11 14:52:48 -0300

    Installed Alamofire and SwiftyJSON. Added GitHubAPIManager.swift file. Print public gists in viewDidAppear

---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+
+use_frameworks!
+
+pod 'SwiftyJSON', :git => 'https://github.com/SwiftyJSON/SwiftyJSON.git', :branch => 'xcode7'
+pod 'Alamofire', '~> 2.0'
---
+++ grokSwiftREST/GitHubAPIManager.swift
@@ -0,0 +1,25 @@
+//
+//  GitHubAPIManager.swift
+//  grokSwiftREST
+//
+//  Created by Christina Moulton on 2015-09-11.
+//  Copyright © 2015 Teak Mobile Inc. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class GitHubAPIManager {
+  static let sharedInstance = GitHubAPIManager()
+  
+  // MARK: - Basic Auth
+  func printPublicGists() -> Void {
+    Alamofire.request(.GET, "https://api.github.com/gists/public")
+      .responseString { _, _, result in
+        if let receivedString = result.value {
+          print(receivedString)
+        }
+    }
+  }
+}

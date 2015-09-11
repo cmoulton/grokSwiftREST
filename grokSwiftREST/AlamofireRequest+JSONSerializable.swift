4c3c90ee4d5f4f86f6004643295856b304400f7f
Christina Moulton <christina@teakmobile.com>
2015-09-11 15:39:27 -0300

    Updated responseObject and responseArray to be more Swift 2.0 like

--- grokSwiftREST/AlamofireRequest+JSONSerializable.swift
+++ grokSwiftREST/AlamofireRequest+JSONSerializable.swift
@@ -13,23 +13,27 @@ import SwiftyJSON
 extension Alamofire.Request {
   public func responseObject<T: ResponseJSONObjectSerializable>(completionHandler: (NSURLRequest?, NSHTTPURLResponse?, Result<T>) -> Void) -> Self {
     let responseSerializer = GenericResponseSerializer<T> { request, response, data in
-      if let responseData = data {
-        let jsonData:AnyObject?
-        do {
-          jsonData = try NSJSONSerialization.JSONObjectWithData(responseData, options: [])
-        } catch  {
-          return .Failure(responseData, error)
-        }
-        
-        let json = SwiftyJSON.JSON(jsonData!)
-        if let newObject = T(json: json) {
-          // TODO: should this be a failable init?
-          return .Success(newObject)
-        }
+      guard let responseData = data else {
+        let failureReason = "Array could not be serialized because input data was nil."
+        let error = Error.errorWithCode(.DataSerializationFailed, failureReason: failureReason)
+        return .Failure(data, error)
       }
-      // TODO: handle & return appropriate error(s)
+      
+      let jsonData: AnyObject?
+      do {
+        jsonData = try NSJSONSerialization.JSONObjectWithData(responseData, options: [])
+      } catch  {
+        let error = Error.errorWithCode(.JSONSerializationFailed, failureReason: "JSON could not be converted to data")
+        return .Failure(responseData, error)
+      }
+      
+      let json = SwiftyJSON.JSON(jsonData!)
+      if let newObject = T(json: json) {
+        // TODO: should this be a failable init?
+        return .Success(newObject)
+      }
       let error = Error.errorWithCode(.JSONSerializationFailed, failureReason: "JSON could not be converted to object")
-      return .Failure(nil, error)
+      return .Failure(responseData, error)
     }
     
     return response(responseSerializer: responseSerializer,
@@ -38,25 +42,28 @@ extension Alamofire.Request {
   
   public func responseArray<T: ResponseJSONObjectSerializable>(completionHandler: (NSURLRequest?, NSHTTPURLResponse?, Result<[T]>) -> Void) -> Self {
     let responseSerializer = GenericResponseSerializer<[T]> { request, response, data in
-      if let responseData = data {
-        let jsonData:AnyObject?
-        do {
-          jsonData = try NSJSONSerialization.JSONObjectWithData(responseData, options: [])
-        } catch  {
-          return .Failure(responseData, error)
-        }
-        var objects: [T] = []
-        let json = SwiftyJSON.JSON(jsonData!)
-        for (_, item) in json {
-          if let object = T(json: item) {
-            objects.append(object)
-          }
-        }
-        return .Success(objects)
+      guard let responseData = data else {
+        let failureReason = "Array could not be serialized because input data was nil."
+        let error = Error.errorWithCode(.DataSerializationFailed, failureReason: failureReason)
+        return .Failure(data, error)
       }
-      // TODO: handle & return appropriate error(s)
-      let error = Error.errorWithCode(.JSONSerializationFailed, failureReason: "JSON could not be converted to object")
-      return .Failure(nil, error)
+      
+      let jsonData: AnyObject?
+      do {
+        jsonData = try NSJSONSerialization.JSONObjectWithData(responseData, options: [])
+      } catch  {
+        let error = Error.errorWithCode(.JSONSerializationFailed, failureReason: "JSON could not be converted to object")
+        return .Failure(responseData, error)
+      }
+      
+      let json = SwiftyJSON.JSON(jsonData!)
+      var objects: [T] = []
+      for (_, item) in json {
+        if let object = T(json: item) {
+          objects.append(object)
+        }
+      }
+      return .Success(objects)
     }
     
     return response(responseSerializer: responseSerializer,
